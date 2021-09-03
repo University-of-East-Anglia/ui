eee702bea8a7d6f3953469c7d9d216e06f0e489e
Tom Stanley <tom.stanley@uea.ac.uk>
2021-09-03 10:49:46 +0100

    Take steps towards being a component library with new build process

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -6,23 +6,23 @@ import InstagramSVG from "./Instagram";
 import "../App.css";
 
 export interface Props {
-    icon: "facebook" | "twitter" | "instagram";
+	icon: "facebook" | "twitter" | "instagram";
 }
 
-export const Hero = (props) => {
-    let iconClass: string = "w-8 h-8";
-    switch (props.icon) {
-        case "facebook":
-            var displayIcon = <FacebookSVG class={iconClass} />;
-            break;
-        case "twitter":
-            var displayIcon = <TwitterSVG class={iconClass} />;
-            break;
-        case "instagram":
-            var displayIcon = <InstagramSVG class={iconClass} />;
-            break;
-        default:
-            return;
-    }
-    return <>{displayIcon}</>;
+export const Hero = (props: any) => {
+	let iconClass: string = "w-8 h-8";
+	switch (props.icon) {
+		case "facebook":
+			var displayIcon = <FacebookSVG class={iconClass} />;
+			break;
+		case "twitter":
+			var displayIcon = <TwitterSVG class={iconClass} />;
+			break;
+		case "instagram":
+			var displayIcon = <InstagramSVG class={iconClass} />;
+			break;
+		default:
+			return;
+	}
+	return <>{displayIcon}</>;
 };
