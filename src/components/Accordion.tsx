95eeb0b01b5baaa249f40e68415fe9300ba4e629
Jamie Platten <jamie.platten@uea.ac.uk>
2021-09-09 11:06:01 +0100

    Just testing

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,16 +1,13 @@
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 interface ComponentProps {}
 
 interface ComponentState {}
 
 export class Accordion extends Component<ComponentProps, ComponentState> {
-	state: ComponentState = {};
+  state: ComponentState = {};
 
-	render(){
-		return (
-			// There is no such thing as magic, just illusion. Things only change when we change them. But you have to do it skillfully, in secret. Then it seems like magic.
-		);
-	}
+  render() {
+    return <div></div>;
+  }
 }
