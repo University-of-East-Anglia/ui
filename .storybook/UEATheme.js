6b08769f4ebc1db2c4324ce730bf245dd75f3dc5
Jamie Platten <jamie.platten@uea.ac.uk>
2022-03-15 20:41:02 +0000

    Theme changes

--- .storybook/UEATheme.js
+++ .storybook/UEATheme.js
@@ -2,6 +2,35 @@ import { create } from "@storybook/theming";
 
 export default create({
   base: "dark",
+
+  colorPrimary: "hotpink",
+  colorSecondary: "deepskyblue",
+
+  // UI
+  appBg: "white",
+  appContentBg: "silver",
+  appBorderColor: "grey",
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: "monospace",
+
+  // Text colors
+  textColor: "black",
+  textInverseColor: "rgba(255,255,255,0.9)",
+
+  // Toolbar default and active colors
+  barTextColor: "silver",
+  barSelectedColor: "black",
+  barBg: "hotpink",
+
+  // Form colors
+  inputBg: "white",
+  inputBorder: "silver",
+  inputTextColor: "black",
+  inputBorderRadius: 4,
+
   brandTitle: "UEA UI Library",
   brandUrl: "https://www.uea.ac.uk",
   brandImage:
