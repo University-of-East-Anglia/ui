6bc7c1f5d3d7fb86e1cb9c7fe0fbe6da53c0a2f2
Jamie Platten <jamie.platten@uea.ac.uk>
2022-04-28 12:52:05 +0100

    Fix issues with Storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,19 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-a11y",
-    "storybook-addon-sass-postcss",
-  ],
-  webpackFinal: async (config) => {
-    config.resolve.extensions.push(".tsx");
-    config.resolve.extensions.push(".ts");
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y"],
+  webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
-      test: /\,css&/,
-      use: [
-        {
-          loader: "postcss-loader",
-          options: {
-            ident: "postcss",
-            plugins: [require("tailwindcss"), require("autoprefixer")],
-          },
-        },
-      ],
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
       include: path.resolve(__dirname, "../"),
     });
+
     return config;
   },
 };
