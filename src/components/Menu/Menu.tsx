24346f7788635158e1f06b7995cc19405e4647db
Jamie Platten <jamie.platten@uea.ac.uk>
2022-07-29 11:20:23 +0100

    set menu urls to relative

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -83,34 +83,30 @@ export class Menu extends Component<Props, ComponentState> {
     };
 
     const menuActive = this.state.menuActive === false ? "" : " open";
-    
+
     return (
       <nav data-swiftype-index="false" className="oc-main">
         <button onClick={menuState} className="" title="Open Menu">
           <svg
-                className="oc-search-icon"
-                focusable="false"
-                role="presentation"
-                viewBox="0 0 712 712"
-              >
-                <path xmlns="http://www.w3.org/2000/svg" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/>
-              </svg>
+            className="oc-search-icon"
+            focusable="false"
+            role="presentation"
+            viewBox="0 0 712 712"
+          >
+            <path
+              xmlns="http://www.w3.org/2000/svg"
+              d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+            />
+          </svg>
           Menu
         </button>
         <div className={"oc-menu-container" + menuActive}>
-
           <ul className="oc-utility-menu" role="menu">
             {this.props.utilityItemArray.map((item) => (
               <li key={item.uuid} className="oc-utility-item" role="none">
-                <a
-                    className=""
-                    href={item.content.url}
-                    role="menuitem"
-                  >
-                    <span className="utility-item-title">
-                      {item.content.title}
-                    </span>
-                  </a>
+                <a className="" href={item.content.url} role="menuitem">
+                  <span className="utility-item-title">{item.content.title}</span>
+                </a>
               </li>
             ))}
           </ul>
@@ -197,7 +193,6 @@ export class Menu extends Component<Props, ComponentState> {
                           <a
                             className=""
                             href={
-                              "https://www.uea.ac.uk" +
                               this.props.navLinkArray.find((x) => x.uuid === item.navItem)?.content
                                 .url
                             }
@@ -249,7 +244,6 @@ export class Menu extends Component<Props, ComponentState> {
                                   <a
                                     className=""
                                     href={
-                                      "https://www.uea.ac.uk" +
                                       this.props.navLinkArray.find((x) => x.uuid === item.navItem)
                                         ?.content.url
                                     }
@@ -300,7 +294,6 @@ export class Menu extends Component<Props, ComponentState> {
                                         <a
                                           className=""
                                           href={
-                                            "https://www.uea.ac.uk" +
                                             this.props.navLinkArray.find(
                                               (x) => x.uuid === item.navItem
                                             )?.content.url
