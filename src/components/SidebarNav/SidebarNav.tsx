f8980eae9bdd9b2b0237fdcc10470a8bd0bbd468
jamieplatten <jamie.platten@uea.ac.uk>
2022-09-02 15:17:18 +0100

    Add menu button title for accessibility reasons

--- src/components/SidebarNav/SidebarNav.tsx
+++ src/components/SidebarNav/SidebarNav.tsx
@@ -28,29 +28,35 @@ export class SidebarNav extends Component<ComponentProps, ComponentState> {
         <nav>
           <button className="toggle-menu" onClick={setMenuState} title="Sidebar Menu">
             {this.state.active === false ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+                <span>Open menu</span>
+              </>
             ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+                <span>Close menu</span>
+              </>
             )}
           </button>
           <ul className={"sidebar-menu " + isActive}>
