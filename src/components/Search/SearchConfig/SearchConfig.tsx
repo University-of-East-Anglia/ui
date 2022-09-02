606ac1ffb510092264a29f42cba3c0d07e3284fe
jamieplatten <jamie.platten@uea.ac.uk>
2022-09-02 13:22:29 +0100

    Resolve bug with NaN on datepicker

--- src/components/Search/SearchConfig/SearchConfig.tsx
+++ src/components/Search/SearchConfig/SearchConfig.tsx
@@ -21,6 +21,15 @@ export const SearchConfig = (props: any) => {
       setToDate(newToDate);
     };
 
+    const clearFromDate = () => {
+      setFromDate(488890800);
+      (document.getElementById("datefrom") as HTMLInputElement).value = "";
+    };
+    const clearToDate = () => {
+      setToDate(Date.now());
+      (document.getElementById("dateto") as HTMLInputElement).value = "";
+    };
+
     console.log(fromDate, toDate);
     return (
       <>
@@ -29,20 +38,52 @@ export const SearchConfig = (props: any) => {
             <label className="ui-heading ui-heading--xs" htmlFor="datefrom">
               Date from:
             </label>
-            <input type="date" name="datefrom" id="datefrom" onChange={convertFromDate} />
+            <div className="ui-date-input">
+              <input type="date" name="datefrom" id="datefrom" onChange={convertFromDate} />
+              <button
+                className="ui-date-input-clear"
+                title="Clear Date From"
+                onClick={clearFromDate}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
           </div>
           <div>
             <label className="ui-heading ui-heading--xs" htmlFor="dateto">
               Date to:
             </label>
-            <input type="date" name="dateto" id="dateto" onChange={convertToDate} />
+            <div className="ui-date-input">
+              <input type="date" name="dateto" id="dateto" onChange={convertToDate} />
+              <button className="ui-date-input-clear" title="Clear Date To" onClick={clearToDate}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
 
         <Configure
           facetingAfterDistinct={true}
           hitsPerPage={props.per_page ? props.per_page : 12}
-          filters={`${props.date_attribute}: ${fromDate} TO ${toDate}`}
+          filters={`${props.date_attribute}: ${isNaN(fromDate as any) ? 488890800 : fromDate} TO ${
+            isNaN(toDate as any) ? Date.now() : toDate
+          }`}
         />
       </>
     );
