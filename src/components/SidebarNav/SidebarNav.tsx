bd1d711f2f7005a443b08f684f29998d43272e28
jamieplatten <jamie.platten@uea.ac.uk>
2022-09-02 15:13:28 +0100

    Sidebar Nav component add open and close on mobile devices

--- src/components/SidebarNav/SidebarNav.tsx
+++ src/components/SidebarNav/SidebarNav.tsx
@@ -26,8 +26,32 @@ export class SidebarNav extends Component<ComponentProps, ComponentState> {
     return (
       <div className="sidebar">
         <nav>
-          <button className="toggle-menu" onClick={setMenuState}>
-            Menu
+          <button className="toggle-menu" onClick={setMenuState} title="Sidebar Menu">
+            {this.state.active === false ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            )}
           </button>
           <ul className={"sidebar-menu " + isActive}>
             {this.props.navItems.map((item) => (
