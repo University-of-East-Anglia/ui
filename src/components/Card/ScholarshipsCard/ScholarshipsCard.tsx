abc86b87b7a9a4e5f945b1d2c96d154554abb923
eyupkureli <eyupkurelii@gmail.com>
2022-09-22 08:33:08 +0100

    scholarships card update

--- src/components/Card/ScholarshipsCard/ScholarshipsCard.tsx
+++ src/components/Card/ScholarshipsCard/ScholarshipsCard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../Card.scss";
-import { CTA } from "../../CTAs/CTA";
 
 export interface Props {
   keyvalues: KeyValues[];
@@ -50,8 +49,20 @@ export const ScholarshipsCard = ({ ...props }: Props) => {
           </div>
         </div>
       </div>
-      <div className="card-cta">
-        <CTA reverse={true} title="Find out more" />
+      <div>
+        <button className="card-button">
+          Find out more
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="cta-chevron--right"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+          </svg>
+        </button>
       </div>
     </div>
   );
