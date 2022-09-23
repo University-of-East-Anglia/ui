d3cc38139848c17ee824834179efbb23076d15ea
eyupkureli <eyupkurelii@gmail.com>
2022-09-23 14:19:42 +0100

    first commit

---
+++ src/components/ScholarshipsContent/ScholarshipsContent.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+import { ScholarshipsContent, Props } from "./ScholarshipsContent";
+
+const meta: Meta = {
+  title: "Scholarships Content",
+  component: ScholarshipsContent,
+};
+
+export default meta;
+
+const Template: Story<Props> = (args) => <ScholarshipsContent {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  header: "Scholarships and Loans",
+  content:
+    "We want the brighest and best students to come to UEA and we don't believe that the cost of university should ever be a barrier to a fantastic education. For this reason we offer almostone hundred different scholarships and bursaries. ",
+};
---
+++ src/components/ScholarshipsContent/ScholarshipsContent.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ScholarshipsContent.scss";
+
+export interface Props {
+  header: string;
+  content: string;
+}
+
+export const ScholarshipsContent = ({ ...props }: Props) => {
+  return (
+    <div className="content-container">
+      <div className="header-container">
+        <h1 className="scholarship-header">{props.header}</h1>
+      </div>
+      <div className="excerpt-container">
+        <p className="scholarship-excerpt">{props.content}</p>
+      </div>
+    </div>
+  );
+};
