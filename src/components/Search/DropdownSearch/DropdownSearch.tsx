6704aca852ace33dc9c02161cca0eaec8480c170
eyupkureli <eyupkurelii@gmail.com>
2022-09-25 18:58:55 +0100

    styling implementation

--- src/components/Search/DropdownSearch/DropdownSearch.tsx
+++ src/components/Search/DropdownSearch/DropdownSearch.tsx
@@ -7,10 +7,12 @@ const customStyles = {
     ...base,
     height: 69,
     minHeight: 69,
-    width: 557,
+    maxWidth: 557,
+    minWidth: 300,
     backgroundColor: "rgba(255, 255, 255, 1)",
     border: "2px solid rgba(242, 242, 242, 1)",
     borderRadius: 6,
+    flex: 1,
   }),
 };
 
@@ -21,13 +23,15 @@ export const DropdownSearch = (props: any) => {
       <div>
         <h1 className="ui-dropdown-search scholarships-title">{props.page_title}</h1>
         <label>
-          <Select
-            className="ui-dropdown-search dropdown-container"
-            options={props.options}
-            onChange={onChange}
-            placeholder={props.placeholder}
-            styles={customStyles}
-          />
+          <div className="select-bar">
+            <Select
+              className="ui-dropdown-search dropdown-container"
+              options={props.options}
+              onChange={onChange}
+              placeholder={props.placeholder}
+              styles={customStyles}
+            />
+          </div>
         </label>
       </div>
     </div>
