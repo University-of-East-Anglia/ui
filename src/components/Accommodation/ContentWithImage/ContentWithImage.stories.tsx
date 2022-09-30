598d7959c29579fa21b80c9761fe4784130ce007
eyupkureli <eyupkurelii@gmail.com>
2022-09-30 13:05:36 +0100

    refactor

--- src/components/Accommodation/ContentWithImage/ContentWithImage.stories.tsx
+++ src/components/Accommodation/ContentWithImage/ContentWithImage.stories.tsx
@@ -19,5 +19,5 @@ Default.args = {
     "Once you've received a conditional or unconditional offer from us you can apply for your accommodation. We'll send you an email at that time, which explains what you need to do to apply. Every first year student is guaranteed a room in our student accommodation. When you apply we'll do our best to place you in your preferred option, but you'll also be asked to give a list of alternatives in case your first option is over-subscribed. ",
   paragraphSecond:
     "When you apply we'll ask if you have a disability, so that we can make provide you with the best accommodation for your needs. Please also let us know you would prefer to live in single-gender accommodation.",
-  image: "https://picsum.photos/400/300",
+  image: "https://picsum.photos/1440/655",
 };
