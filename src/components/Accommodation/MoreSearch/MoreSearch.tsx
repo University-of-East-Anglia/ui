31b08044bfa39be2a708e36a1a36f2d480e8245d
eyupkureli <eyupkurelii@gmail.com>
2022-10-08 14:44:50 +0100

    icon added

--- src/components/Accommodation/MoreSearch/MoreSearch.tsx
+++ src/components/Accommodation/MoreSearch/MoreSearch.tsx
@@ -73,7 +73,21 @@ export const MoreSearch = (props: any) => {
             </div>
             <div className="select-container-without-image">
               <div className="smalltitle-without-image-container">
-                <h1 className="smalltitle-without-image">{props.price_title}</h1>
+                <h1 className="smalltitle-without-image">
+                  {props.price_title}{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    fill="currentColor"
+                    className="bi bi-question-circle"
+                    viewBox="0 0 16 16"
+                  >
+                    {" "}
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />{" "}
+                    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />{" "}
+                  </svg>
+                </h1>
               </div>
               <form>
                 <label>
@@ -94,7 +108,21 @@ export const MoreSearch = (props: any) => {
           <div className="search-container-column-right">
             <div className="select-container-without-image">
               <div className="smalltitle-without-image-container">
-                <h1 className="smalltitle-without-image">{props.shared_title}</h1>
+                <h1 className="smalltitle-without-image">
+                  {props.shared_title}{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    fill="currentColor"
+                    className="bi bi-question-circle"
+                    viewBox="0 0 16 16"
+                  >
+                    {" "}
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />{" "}
+                    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />{" "}
+                  </svg>
+                </h1>
               </div>
               <form>
                 <label>
@@ -110,7 +138,21 @@ export const MoreSearch = (props: any) => {
             </div>
             <div className="select-container-without-image">
               <div className="smalltitle-without-image-container">
-                <h1 className="smalltitle-without-image">{props.study_title}</h1>
+                <h1 className="smalltitle-without-image">
+                  {props.study_title}{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    fill="currentColor"
+                    className="bi bi-question-circle"
+                    viewBox="0 0 16 16"
+                  >
+                    {" "}
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />{" "}
+                    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />{" "}
+                  </svg>
+                </h1>
               </div>
               <form>
                 <label>
@@ -131,7 +173,21 @@ export const MoreSearch = (props: any) => {
             </div>
             <div className="select-container-without-image">
               <div className="smalltitle-without-image-container">
-                <h1 className="smalltitle-without-image">{props.family_title}</h1>
+                <h1 className="smalltitle-without-image">
+                  {props.family_title}{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    fill="currentColor"
+                    className="bi bi-question-circle"
+                    viewBox="0 0 16 16"
+                  >
+                    {" "}
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />{" "}
+                    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />{" "}
+                  </svg>
+                </h1>
               </div>
               <form>
                 <label>
