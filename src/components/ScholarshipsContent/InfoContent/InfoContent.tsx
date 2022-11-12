9cc25e36342fe81704e1df2793a4fc3417ad4135
eyupkureli <nzf22fju@uea.ac.uk>
2022-11-12 13:41:41 +0000

    info content css refactor

--- src/components/ScholarshipsContent/InfoContent/InfoContent.tsx
+++ src/components/ScholarshipsContent/InfoContent/InfoContent.tsx
@@ -14,23 +14,25 @@ export interface Contents {
 
 export const InfoContent = (props: Props) => {
   return (
-    <div className="info-container">
-      <h1 className="info-header">Government Funding</h1>
-      {props.contents.map((item, i) => (
-        <div key={i} className="item container">
-          <h1 className="item-title">{item.header}</h1>
-          <p className="item-content">{item.content}</p>
+    <section>
+      <div className="info-container">
+        <h1 className="info-header">Government Funding</h1>
+        {props.contents.map((item, i) => (
+          <div key={i} className="item container">
+            <h1 className="item-title">{item.header}</h1>
+            <p className="item-content">{item.content}</p>
+          </div>
+        ))}
+        <div className="link-container">
+          <p className="info-text">
+            Use the{" "}
+            <a className="info-link" href={props.link}>
+              {props.anchor}
+            </a>{" "}
+            to check your eligibility for government funding.
+          </p>
         </div>
-      ))}
-      <div className="link-container">
-        <p className="info-text">
-          Use the{" "}
-          <a className="info-link" href={props.link}>
-            {props.anchor}
-          </a>{" "}
-          to check your eligibility for government funding.
-        </p>
       </div>
-    </div>
+    </section>
   );
 };
