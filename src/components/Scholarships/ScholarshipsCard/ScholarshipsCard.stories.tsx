83ca4a1f3f65a42d0f16953d44b23780bab54816
Ollie Watkins <nmd12kzu@uea.ac.uk>
2022-12-02 09:24:58 +0000

    Scholarship card
    Changes to all scholarship card props and a few style tweaks to the button with some minor layout changes; also made a small change to the CTA SCSS so the button chevron remains good.

--- src/components/Scholarships/ScholarshipsCard/ScholarshipsCard.stories.tsx
+++ src/components/Scholarships/ScholarshipsCard/ScholarshipsCard.stories.tsx
@@ -13,14 +13,97 @@ const Template: Story<Props> = (args) => <ScholarshipsCard {...args} />;
 
 export const Default = Template.bind({});
 
+// Default.args = {
+//   keyvalues: [
+//     { itemtitle: "Value", itemdata: "£3,000" },
+//     { itemtitle: "Number of years", itemdata: "3" },
+//     { itemtitle: "Total Value", itemdata: "£9,000" },
+//     { itemtitle: "Level", itemdata: "Undergraduate" },
+//     { itemtitle: "Applicant Nationality", itemdata: "UK" },
+//   ],
+//   title: "Difference Undergraduate",
+//   header: "Scholarship",
+// };
+
 Default.args = {
-  keyvalues: [
-    { itemtitle: "Value", itemdata: "£3,000" },
-    { itemtitle: "Number of years", itemdata: "3" },
-    { itemtitle: "Total Value", itemdata: "£9,000" },
-    { itemtitle: "Level", itemdata: "Undergraduate" },
-    { itemtitle: "Applicant Nationality", itemdata: "UK" },
-  ],
-  title: "Difference Undergraduate",
-  header: "Scholarship",
-};
+  OfficialNameOfScholarship: "MA Educational Practice and Research Scholarship",
+  TotalValue: "2815",
+  SchoolFaculty: "Education",
+  Years: "2023",
+  FundingType: "Tuition Fees",
+  URLforDetailsPage: "/study/fees-and-funding/scholarships-finder/scholarships-a-z/ma-educational-practice-and-research-scholarship",
+  ApplicantsNationality: "Any",
+  TypeOfCourse: "Postgraduate Taught",
+}
+
+  // Body:
+  //   {
+  //     "_uid": "ed6b1cc5-356a-42a2-821b-7d2f624230dd",
+  //     "Years": "2023",
+  //     "Course": "",
+  //     "Subject": "",
+  //     "Criteria": "",
+  //     "component": "GS030",
+  //     "Categories": [],
+  //     "TotalValue": "2815",
+  //     "AnnualValue": "",
+  //     "FundingType": "Tuition Fees",
+  //     "ResultsPanel": [],
+  //     "TypeOfCourse": "Postgraduate Taught",
+  //     "CountryRegion": "Norfolk/Suffolk",
+  //     "SchoolFaculty": "Education",
+  //     "FurtherCriteria": "You are working in a UEA target Outreach school. A full list of these are listed below.",
+  //     "TotalValueRange": "",
+  //     "CountryofDomicile": "Any",
+  //     "URLforDetailsPage": "/study/fees-and-funding/scholarships-finder/scholarships-a-z/ma-educational-practice-and-research-scholarship",
+  //     "AcademicExcellence": "",
+  //     "ApplicationProcess": "",
+  //     "TermsAndConditions": {
+  //       "type": "doc",
+  //       "content": [
+  //         {
+  //           "type": "paragraph",
+  //           "content": [
+  //             {
+  //               "text": "General Terms and Conditions",
+  //               "type": "text",
+  //               "marks": [
+  //                 {
+  //                   "type": "link",
+  //                   "attrs": {
+  //                     "href": "https://www.uea.ac.uk/study/fees-and-funding/scholarships-finder/general-terms-and-conditions",
+  //                     "uuid": null,
+  //                     "anchor": null,
+  //                     "target": null,
+  //                     "linktype": "url"
+  //                   }
+  //                 },
+  //                 {
+  //                   "type": "bold"
+  //                 },
+  //                 {
+  //                   "type": "underline"
+  //                 }
+  //               ]
+  //             }
+  //           ]
+  //         }
+  //       ]
+  //     },
+  //     "RelatedScholarships": "",
+  //     "AdditionalInformation": {
+  //       "type": "doc",
+  //       "content": [
+  //         {
+  //           "type": "paragraph"
+  //         }
+  //       ]
+  //     },
+  //     "ApplicantsNationality": "Any",
+  //     "NumberAvailablePerYear": "",
+  //     "URLforDetailsPageTarget": false,
+  //     "OfficialNameOfScholarship": "MA Educational Practice and Research Scholarship",
+  //     "RestrictionsWithOtherScholarships": "",
+  //     "_editable": "<!--#storyblok#{\"name\": \"GS030\", \"space\": \"154676\", \"uid\": \"ed6b1cc5-356a-42a2-821b-7d2f624230dd\", \"id\": \"225821753\"}-->"
+  //   },
+// };
--- src/components/Scholarships/ScholarshipsCard/ScholarshipsCard.tsx
+++ src/components/Scholarships/ScholarshipsCard/ScholarshipsCard.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import "../../Card/Card.scss";
 
 export interface Props {
-  keyvalues: KeyValues[];
-  title: string;
-  header: string;
-}
-
-export interface KeyValues {
-  itemtitle: string;
-  itemdata: string;
+  cardHeading?: string;
+  OfficialNameOfScholarship?: string;
+  TotalValue?: string;
+  SchoolFaculty?: string;
+  Years?: string;
+  FundingType?: string;
+  URLforDetailsPage?: string;
+  ApplicantsNationality?: string;
+  TypeOfCourse?: string;
 }
 
 export const ScholarshipsCard = ({ ...props }: Props) => {
+
   return (
     <div className="card scholarships-card">
       <div className="card-header-container">
@@ -31,42 +33,73 @@ export const ScholarshipsCard = ({ ...props }: Props) => {
             clipRule="evenodd"
           />
         </svg>
-        <span className="card-heading">{props.header}</span>
+        <span className="card-heading">{props.cardHeading}</span>
       </div>
       <div className="card-content">
         <div className="card-text">
           <div className="card-title-container">
-            <h3 className="card-title">{props.title}</h3>
+            <h3 className="card-title">{props.OfficialNameOfScholarship}</h3>
           </div>
           <div className="key-values">
-            {props.keyvalues.map((value, i) => (
+            {props.ApplicantsNationality &&
               <div className="row">
                 <div className="column">
-                  <span>{value.itemtitle}</span>
+                  <span>Nationality</span>
                 </div>
                 <div className="column vertical">
-                  <span>{value.itemdata}</span>
+                  <span>{props.ApplicantsNationality}</span>
                 </div>
               </div>
-            ))}
+            }
+            {props.TypeOfCourse &&
+              <div className="row">
+                <div className="column">
+                  <span>Level of Study</span>
+                </div>
+                <div className="column vertical">
+                  <span>{props.TypeOfCourse}</span>
+                </div>
+              </div>
+            }
+            {props.Years &&
+              <div className="row">
+                <div className="column">
+                  <span>Intake</span>
+                </div>
+                <div className="column vertical">
+                  <span>{props.Years}</span>
+                </div>
+              </div>
+            }
+            {props.TotalValue &&
+              <div className="row">
+                <div className="column">
+                  <span>Value</span>
+                </div>
+                <div className="column vertical">
+                  <span><span>£</span>{props.TotalValue}</span>
+                </div>
+              </div>}
           </div>
         </div>
       </div>
       <div>
-        <button className="card-button" aria-label="find out more">
-          Find out more
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="cta-chevron--right"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-            aria-hidden="true"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-          </svg>
-        </button>
+        <a href={props.URLforDetailsPage}>
+          <button className="card-button" aria-label="find out more">
+            <span>Find out more</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="cta-chevron--right"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+              aria-hidden="true"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+            </svg>
+          </button>
+        </a>
       </div>
     </div>
   );
