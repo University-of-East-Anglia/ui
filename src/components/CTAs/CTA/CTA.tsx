ebb1bd28944c53d2a715f2ba9e60b5a8c330692f
Jamie Platten <jamie.platten@uea.ac.uk>
2023-01-10 14:15:09 +0000

    Add the href to the CTA attributes

--- src/components/CTAs/CTA/CTA.tsx
+++ src/components/CTAs/CTA/CTA.tsx
@@ -2,32 +2,32 @@ import React, { Component } from "react";
 import "../CTA.scss";
 
 export interface Props {
-    title: string;
-    link: string;
-    reverse?: boolean;
+  title: string;
+  link: string;
+  reverse?: boolean;
 }
 
 interface ComponentState {}
 
 export class CTA extends Component<Props, ComponentState> {
-    state: ComponentState = {};
+  state: ComponentState = {};
 
-    render() {
-        const colorClass = this.props.reverse != true ? "-reverse" : "-pink";
-        return (
-            <a className={"cta cta-default cta-default" + colorClass}>
-                {this.props.title}
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="cta-chevron--right"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                >
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-                </svg>
-            </a>
-        );
-    }
+  render() {
+    const colorClass = this.props.reverse != true ? "-reverse" : "-pink";
+    return (
+      <a className={"cta cta-default cta-default" + colorClass} href={this.props.link}>
+        {this.props.title}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="cta-chevron--right"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+        </svg>
+      </a>
+    );
+  }
 }
