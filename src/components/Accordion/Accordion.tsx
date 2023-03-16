ba6e503b6d95c6def4e24306e03d2348adda1ba5
Jamie Platten <jamie.platten@uea.ac.uk>
2023-03-16 14:46:09 +0000

    Updates to accordion
    Accessibility updates to accordion component

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -2,56 +2,66 @@ import React, { Component } from "react";
 import "./Accordion.scss";
 
 export interface Props {
-    accordion: AccordionContent[];
+  accordion: AccordionContent[];
 }
 
 export interface AccordionContent {
-    title: string;
-    content: string;
+  title: string;
+  content: string;
 }
 
 interface ComponentState {
-    active: number;
+  active: number[];
 }
 
 export class Accordion extends Component<Props, ComponentState> {
-    state: ComponentState = {
-        active: 0,
+  state: ComponentState = {
+    active: [0],
+  };
+
+  render() {
+    const toggleClass = (e: any) => {
+      const headerID = document.getElementById(e.target.id);
+      const bodyID = document.getElementById(
+        e.target.id.replace("accordion-title", "accordion-content")
+      );
+      const headerClass: any = "accordion--header-open";
+      const bodyClass: any = "accordion--panel-open";
+
+      if (headerID?.classList.contains(headerClass)) {
+        headerID.classList.remove(headerClass);
+      } else {
+        headerID?.classList.add(headerClass);
+      }
+      if (bodyID?.classList.contains(bodyClass)) {
+        bodyID.classList.remove(bodyClass);
+      } else {
+        bodyID?.classList.add(bodyClass);
+      }
     };
+    return (
+      <div className="accordion">
+        {this.props.accordion.map((pane: any, index: number) => (
+          <div key={index}>
+            <button
+              id={"accordion-title" + index}
+              className="accordion--header"
+              onClick={toggleClass}
+            >
+              {pane.title}
+            </button>
 
-    render() {
-        return (
-            <div className="accordion">
-                {this.props.accordion.map((pane: any, index) => (
-                    <div key={index}>
-                        <div
-                            id={"accordion-title" + index}
-                            className={
-                                this.state.active === index
-                                    ? "accordion--header accordion--header-open"
-                                    : "accordion--header"
-                            }
-                            role="button"
-                            onClick={() => this.setState({ active: index })}
-                        >
-                            {pane.title}
-                        </div>
-
-                        <div
-                            id={"accordion-content" + index}
-                            role="region"
-                            aria-labelledby={"accordion-title" + index}
-                            className={
-                                this.state.active === index
-                                    ? "accordion--panel-open"
-                                    : "accordion--panel"
-                            }
-                        >
-                            {pane.content}
-                        </div>
-                    </div>
-                ))}
+            <div
+              id={"accordion-content" + index}
+              role="region"
+              aria-labelledby={"accordion-title" + index}
+              className="accordion--panel"
+            >
+              {pane.content}
             </div>
-        );
-    }
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
