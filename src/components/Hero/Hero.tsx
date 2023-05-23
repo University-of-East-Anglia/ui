73dc8dc76fecfb219bb55fba073eda361011f776
Ollie Watkins <nmd12kzu@uea.ac.uk>
2023-05-23 13:10:38 +0100

    Adding srcSet prop to the hero banner
    Allowing the hero banner to accept an HTMLImageElement into srcSet

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,13 +11,15 @@ export interface Props {
     alt: string;
     position: "middle" | "bottom";
     subtext?: string;
+    srcSet?: HTMLImageElement;
 }
 
 export const Hero = ({ header, ...props }: Props) => {
 
     return (
         <div className="hero-container">
-            <Image className={props.variant} src={props.image} alt={props.alt}/>
+            {props.image && <Image className={props.variant} src={props.image} alt={props.alt} />}
+            {props.srcSet && props.srcSet}
             <div className={`header ${props.position}`}>
                 <div>
                     <h1 className={props.uppercase ? 'uppercase' : ''}>
